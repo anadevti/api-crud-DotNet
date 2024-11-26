87d42257c38e079f9a6e11d7e3f981e40e557cdc
Ana Souza | Stone <ana.carolyne@stone.com.br>
2024-11-26 18:25:44 -0300

    adicionando conexão ao db sqlite

--- MyApiV2/Data/AppDbContext.cs
+++ MyApiV2/Data/AppDbContext.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyApiV2.Studants;
 
 namespace MyApiV2.Data;
 
 public class AppDbContext : DbContext
 {
-    
+    private DbSet<Studant> Studants { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(connectionString:"Data Source=app.db");
+        base.OnConfiguring(optionsBuilder);
+    }
 }
